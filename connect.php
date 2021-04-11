4781ce379ed6514a1fec2e16b8265c54aba334b5
Resheil Agarwal <resheil@resheil.com>
2021-04-12 03:19:54 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,15 @@
 <?php
+/*
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "granimart";
+*/
+
+$servername = "localhost";
+$username = "portalw_stoneown";
+$password = "6w@eW6s&@g~m";
+$dbname = "portalw_stonemart";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);

9bea161df672dec4657174d234844aa6a2b68691
Resheil Agarwal <resheil@resheil.com>
2021-04-13 02:29:40 +0530

    Update 2.php

--- elements/banners/2.php
+++ elements/banners/2.php
@@ -1,13 +1,13 @@
 <!----------- BAnner ------------->
 
-
+      <div class="carousel-item active" ">
 <!-- Full Page Image Header with Vertically Centered Content -->
-<header class="masthead">
+<header class="masthead" style="background-image: url('<?php echo $prefix_image_banner.$content_banner["banner_image_1"]; ?>')">
   <div class="container h-100">
     <div class="row h-100 align-items-center">
       <div class="col-12 text-center">
-        <h1 class="font-weight-light">Vertically Centered Masthead Content</h1>
-        <p class="lead">A great starter layout for a landing page</p>
+        <h1 class="font-weight-light">Welcome !</h1>
+        <p class="lead"><?php echo $content_banner["banner_text_1"]; ?></p>
       </div>
     </div>
   </div>

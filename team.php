1bff61f6e31b98c2176a3a99d49560bdcba1966c
Resheil Agarwal <resheil@resheil.com>
2021-04-22 03:04:36 +0530

    Changing the title to dynamic

--- team.php
+++ team.php
@@ -9,7 +9,7 @@ include("get_design.php");
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title><?php echo ucfirst($content_general[0]["domain_name"]); ?>  - Team</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">  
